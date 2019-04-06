d001044d88b1484783f37f984150a799f389c2e5
vyava <zafergenc02@gmail.com>
2019-04-06 14:31:07 +0300

    marker düzenlendi

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 declare var google;
-
+declare var MarkerClusterer;
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
@@ -7,78 +7,97 @@ function initMap() {
   });
 
   map.data.loadGeoJson(
-    'https://storage.googleapis.com/mapsdevsite/json/google.json');
+    "https://storage.googleapis.com/mapsdevsite/json/google.json"
+  );
   return map;
 }
 
 var map;
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
+  // Get elementlist
+  let bayiler = getElementList();
+
+  // Init map to the #map element
+  map = initMap();
+
+  // Iterate for Coords
+  let coordsIterator = new Iterate(bayiler);
+  // Iterate coords array
+  // let coordsArray = [];
+  let markerArray = [];
+  let coords = null;
+  while ((coords = coordsIterator.next()) != null) {
+    // Get coords from addres
+    let [lat, lng]: [number, number] = await getLatLng(coords.adres);
+    // Set marker to the Map
+    // coordsArray.push([lat, lng]);
+    let marker = setMarkers(map, ["coords.bayiAdi", lat, lng, 100]);
+    markerArray.push(marker);
+  }
+
+  console.log(markerArray);
+  // console.log(coordsArray);
+
+  var markerCluster = new MarkerClusterer(map, markerArray, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+  });
+});
+
+function getElementList() {
   let bayiler = [];
   let elements = [].slice.call(
     document.getElementsByClassName("list-group-item")
   );
 
-  // Iterate for elements
+  // Iterate instance
   let elementArray = new Iterate(elements);
   let element = null;
+
+  // Iterate for elements
   while ((element = elementArray.next()) != null) {
     let bayiInfo = JSON.parse(element.getAttribute("data-info"));
 
     bayiler.push(bayiInfo);
   }
 
-  // Init map to the #map element
-  map = initMap();
-
-  // Iterate for Coords
-  let coordsArray = new Iterate(bayiler);
-
-  // Iterate coords array
-  let coords = null;
-  while ((coords = coordsArray.next()) != null) {
-    // Get coords from addres
-    getLatLng(coords.adres, _coords => {
-      // Set marker to the Map
-      setMarkers(map, ["coords.bayiAdi", _coords[0], _coords[1], 100]);
-    });
-  }
-});
+  return bayiler;
+}
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
 
-function getLatLng(adres, fn) {
-  var geocoder;
-  if (geocoder == undefined) {
-    geocoder = new google.maps.Geocoder();
-  } else {
-    console.log("zaten var");
-  }
-
-  // Geocode an address.
-  return geocoder.geocode(
-    {
-      address: adres
-    },
-    (result, status) => {
-      if (status == "OK") {
-        var lat = result[0].geometry.location.lat();
-        var lng = result[0].geometry.location.lng();
-        var coords = [lat, lng];
-        fn(coords);
-      } else {
-        fn(status);
-      }
+function getLatLng(adres): Promise<any> {
+  return new Promise((resolve, reject) => {
+    var geocoder;
+    if (geocoder == undefined) {
+      geocoder = new google.maps.Geocoder();
+    } else {
+      console.log("zaten var");
     }
-  );
+
+    // Geocode an address.
+    geocoder.geocode(
+      {
+        address: adres
+      },
+      (result, status) => {
+        if (status == "OK") {
+          var lat = result[0].geometry.location.lat();
+          var lng = result[0].geometry.location.lng();
+          var coords: number[] = [lat, lng];
+          resolve(coords);
+        } else {
+          reject(status);
+        }
+      }
+    );
+  });
 }
 
-function setMarkers(map, beach) {
+function setMarkers(map, [title, lat, lng, index]) {
   // Adds markers to the map.
-
-  // Marker sizes are expressed as a Size of X,Y where the origin of the image
-  // (0,0) is located in the top left of the image.
 
   // Origins, anchor positions and coordinates of the marker increase in the X
   // direction to the right and in the Y direction down.
@@ -92,36 +111,46 @@ function setMarkers(map, beach) {
     // The anchor for this image is the base of the flagpole at (0, 32).
     anchor: new google.maps.Point(0, 32)
   };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-  var shape = {
-    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-    type: "poly"
-  };
-  var beach;
+
+  // var shape = {
+  //   coords: [1, 1, 1, 20, 18, 20, 18, 1],
+  //   type: "poly"
+  // };
   var marker = new google.maps.Marker({
-    position: { lat: beach[1], lng: beach[2] },
+    position: { lat, lng },
     map: map,
-    icon: image,
-    labels: 11,
-    shape: shape,
-    title: beach[0],
-    zIndex: beach[3]
+    // icon: {
+    //   path: google.maps.SymbolPath.CIRCLE, //or any others
+    //   scale: 0
+    // },
+    labels: {
+      fontFamily: "FontAwesome",
+      text: "\uf140", //code for font-awesome icon
+      fontSize: "15px",
+      color: "blue"
+    },
+    // shape: shape,
+    id: "id",
+    // title: title,
+    // zIndex: index
   });
 
-  let html = `<h1>Gizli Mesaj</h1>`
+  let html = `<h1>Gizli Mesaj</h1>`;
+
   attachSecretMessage(marker, html);
   //   marker.setAttribute("id", "ddd")
+
+  // marker.set("id", 12)
   marker.addListener("click", e => {
     markerClickHandler(marker, e.latLng);
   });
+
+  return marker;
 }
 
 function markerClickHandler(marker, latLng) {
-  // map.setZoom(8);
+  console.log(marker);
   map.panTo(latLng);
-  // console.log(e)
 }
 
 function attachSecretMessage(marker, secretMessage) {
@@ -131,6 +160,11 @@ function attachSecretMessage(marker, secretMessage) {
 
   marker.addListener("click", function() {
     infowindow.open(marker.get("map"), marker);
+  });
+
+  marker.addListener("mouseover", function() {
+    console.log(this);
+    // infowindow.open(marker.get("map"), marker);
   });
 }
 
